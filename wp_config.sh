af1f19bd29c6e6cdac1005b11a86088c4d1ec9c8
Y-C-WONG <141507805+Y-C-WONG@users.noreply.github.com>
2023-10-14 23:10:54 +0100

    Update wp_config.sh

--- wp_config.sh
+++ wp_config.sh
@@ -9,7 +9,7 @@ MONTHLY_FILE="shgh_wp_"$YYYYMMM".tar.gz"    # Monthly backup archive file name
 DAILY_FILE="shgh_wp_"$YYYYMMDD".tar.gz"    # Daily backup archive file name 
 # Need to update below accordingly
 BACKUP_DIR="/home/bak/"    # Backup archive file location
-ALL_BAK_DIR="/home/bak/monthly/"
+ALL_BAK_DIR="/home/bak/monthly/"  # Monthly backup archive file location
 DB_BACKUP_DIR=$BACKUP_DIR    # DB backup .sql location
 SQL_FILE="shgh_wp_"$YYYYMMDD".sql"
 DB_BACKUP_FILE=$DB_BACKUP_DIR$SQL_FILE    # DB backup .sql file name and location
